d7068a76cba762252c9d61304e3dab78d9cd9113
Sanskar Verma <sanskar.verma@bounteous.com>
2025-05-26 12:35:55 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -39,5 +39,28 @@ public class Main{
         printArray(even);
         System.out.print("Odd Array contains: ");
         printArray(odd);
+
+
+                int num = 29;
+                boolean flag = false;
+
+                // 0 and 1 are not prime numbers
+                if (num == 0 || num == 1) {
+                    flag = true;
+                }
+
+                for (int i = 2; i <= num / 2; ++i) {
+
+                    // condition for nonprime number
+                    if (num % i == 0) {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag)
+                    System.out.println(num + " is a prime number.");
+                else
+                    System.out.println(num + " is not a prime number.");
+            }
     }
-}
